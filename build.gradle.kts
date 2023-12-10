8f55e9fc7df862002aa6a1860729686f79c9bdcc
Sandip Chitale <sandipchitale@gmail.com>
2023-12-09 17:30:27 -0800

    Update for IntelliJ 233.* .

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "sandipchitale"
-version = "1.0.6"
+version = "1.0.7"
 
 repositories {
     mavenCentral()
@@ -32,7 +32,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("222")
-        untilBuild.set("232.*")
+        untilBuild.set("233.*")
     }
 
     signPlugin {
