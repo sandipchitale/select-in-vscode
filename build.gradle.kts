d3e4fbad848da9c87b6f00fa0bfb8dcdc8433bca
sandipchitale@gmail.com <sandipchitale@gmail.com>
2025-04-18 21:15:52 -0700

    Update for 2.x.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,30 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.24"
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.5.0"
 }
 
 group = "sandipchitale"
-version = "1.0.11"
+version = "1.0.12"
 
 repositories {
     mavenCentral()
+
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2022.2.5")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf(/* Plugin Dependencies */))
+dependencies {
+    intellijPlatform {
+        if (project.hasProperty("runIde_ideDir")) {
+            local("${project.extra["runIde_ideDir"]}")
+        } else {
+            intellijIdeaCommunity("2024.2")
+        }
+    }
 }
+
 
 tasks {
     // Set the JVM compatibility versions
@@ -30,14 +36,8 @@ tasks {
         kotlinOptions.jvmTarget = "17"
     }
 
-    runIde {
-        if (project.hasProperty("runIde_ideDir")) {
-            ideDir = file("${project.extra["runIde_ideDir"]}")
-        }
-    }
-
     patchPluginXml {
-        sinceBuild.set("222")
+        sinceBuild.set("242")
         untilBuild.set("251.*")
     }
 
